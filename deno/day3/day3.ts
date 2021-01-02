ade0466706a84d0a98b68d467c882a9b2e36bc40
Matthias <matthias@blex.dev>
2021-01-02 15:21:56 +0100

    add solution 3_1

--- deno/day3/day3.ts
+++ deno/day3/day3.ts
@@ -1,6 +1,6 @@
 import { readLines } from 'https://deno.land/std@0.83.0/io/mod.ts';
 
-const filename = './files/task2.txt';
+const filename = './files/task3.txt';
 const fileReader = await Deno.open(filename);
 
 const lines: string[] = [];
@@ -8,3 +8,17 @@ const lines: string[] = [];
 for await (const line of readLines(fileReader)) {
   lines.push(line);
 }
+
+const hitSomething = (line: string, right: number): boolean => {
+  return getCharFromInfinityLine(line, right) === '#';
+};
+
+const getCharFromInfinityLine = (line: string, number: number): string => {
+  return line[number % line.length];
+};
+
+console.log('Task 1:');
+console.log(
+  lines.map((line, index) => hitSomething(line, index * 3)).filter(Boolean)
+    .length
+);
