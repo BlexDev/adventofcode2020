2451cd81c958fc9466b2d024c89df92f156a3f09
Matthias <matthias@blex.dev>
2021-01-04 13:59:09 +0100

    add solution for 5_1

---
+++ deno/day5/day5.ts
@@ -0,0 +1,42 @@
+import { readLines } from 'https://deno.land/std@0.83.0/io/mod.ts';
+
+const filename = './files/task5.txt';
+const fileReader = await Deno.open(filename);
+
+const lines: string[] = [];
+
+for await (const line of readLines(fileReader)) {
+  lines.push(line);
+}
+
+const getRow = (code: string): number => {
+  return parseInt(
+    code
+      .slice(0, 7 - code.length)
+      .replaceAll('F', '0')
+      .replaceAll('B', '1'),
+    2
+  );
+};
+
+const getColumn = (code: string): number => {
+  return parseInt(
+    code
+      .substr(code.length - 3)
+      .replaceAll('L', '0')
+      .replaceAll('R', '1'),
+    2
+  );
+};
+
+const getId = (code: string): number => {
+  return getRow(code) * 8 + getColumn(code);
+};
+
+console.log('First part: ');
+console.log(
+  lines
+    .map(getId)
+    .sort((a, b) => a - b)
+    .pop()
+);
